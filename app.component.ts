7dbdc3864e28f0c47954e2b735fcc431bc38bd4f
Gerard Bourguett <gerard.bourguett@gmail.com>
2023-05-04 09:56:56 -0400

    first commit

---
+++ app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin]
+  }
+}
